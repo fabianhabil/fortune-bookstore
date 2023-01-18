8b466994246a5e331a86a09e6cc082516cddbb31
fabianhabil <fabianhabilramdhan@gmail.com>
2023-01-19 02:19:09 +0700

    installed new font

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,30 +3,80 @@ import type { EmotionCache } from '@emotion/utils';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import type { FunctionComponent, PropsWithChildren } from 'react';
+import { useEffect } from 'react';
+import type { FunctionComponent, PropsWithChildren, ReactNode } from 'react';
 import createEmotionCache from '@/styles/createEmotionCache';
+import { injectStyle } from 'react-toastify/dist/inject-style';
+import { ToastContainer } from 'react-toastify';
+import theme from '@/styles/theme';
+import type { NextPage } from 'next';
 // eslint-disable-next-line import/extensions
 import '@/styles/globals.css';
-import theme from '@/styles/theme';
 
+type Page<P = unknown> = NextPage<P> & {
+    getLayout?: (_page: ReactNode) => ReactNode;
+};
 interface MyAppProps extends AppProps {
     emotionCache?: EmotionCache;
+    Component: Page;
 }
 
 const clientSideEmotionCache: EmotionCache = createEmotionCache();
 
-const MyApp: FunctionComponent<MyAppProps> = (props: PropsWithChildren<MyAppProps>) => {
-    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+const MyApp: FunctionComponent<MyAppProps> = (
+    props: PropsWithChildren<MyAppProps>
+) => {
+    const {
+        Component,
+        emotionCache = clientSideEmotionCache,
+        pageProps
+    } = props;
+
+    const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            injectStyle();
+        }
+    }, []);
 
     return (
         <CacheProvider value={emotionCache}>
-            <Head>
-                <meta name='viewport' content='initial-scale=1, width=device-width' />
-            </Head>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <Component {...pageProps} />
-            </ThemeProvider>
+            {getLayout(
+                <>
+                    <Head>
+                        <meta
+                            name='description'
+                            content='Fortune Bookstore is a fictional bookstore created for final project database'
+                        />
+                        <meta
+                            name='viewport'
+                            content='initial-scale=1, width=device-width'
+                        />
+                        <meta property='og:type' content='website' />
+                        <meta property='og:title' content='Fortune Bookstore' />
+                        <meta
+                            property='og:description'
+                            content='Fortune Bookstore'
+                        />
+                    </Head>
+                    <ThemeProvider theme={theme}>
+                        <CssBaseline />
+                        <Component {...pageProps} />
+                        <ToastContainer
+                            position='top-right'
+                            autoClose={5000}
+                            hideProgressBar={false}
+                            newestOnTop={true}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover
+                        />
+                    </ThemeProvider>
+                </>
+            )}
         </CacheProvider>
     );
 };
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,7 +1,11 @@
 import type { EmotionCache } from '@emotion/cache';
 import type { EmotionCriticalToChunks } from '@emotion/server/create-instance';
 import createEmotionServer from '@emotion/server/create-instance';
-import type { AppPropsType, DocumentInitialProps, RenderPage } from 'next/dist/shared/lib/utils';
+import type {
+    AppPropsType,
+    DocumentInitialProps,
+    RenderPage
+} from 'next/dist/shared/lib/utils';
 import type { DocumentContext } from 'next/document';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import type { NextRouter } from 'next/router';
@@ -15,12 +19,24 @@ export default class MyDocument extends Document {
         return (
             <Html lang='en'>
                 <Head>
-                    <link rel='preconnect' href='https://fonts.googleapis.com' />
-                    <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
+                    <link
+                        rel='preconnect'
+                        href='https://fonts.googleapis.com'
+                    />
+                    <link
+                        rel='preconnect'
+                        href='https://fonts.gstatic.com'
+                        crossOrigin='anonymous'
+                    />
                     <link
                         href='https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800&display=swap'
                         rel='stylesheet'
                     />
+                    <link
+                        href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap'
+                        rel='stylesheet'
+                    />
+                    <link rel='icon' href='/favicon.ico' />
                 </Head>
                 <body>
                     <Main />
@@ -43,21 +59,28 @@ MyDocument.getInitialProps = async (ctx: DocumentContext) => {
     /* eslint-disable */
     ctx.renderPage = () =>
         originalRenderPage({
-            enhanceApp: (App: any) => (props: AppPropsType<NextRouter, {}>) => <App emotionCache={cache} {...props} />
+            enhanceApp: (App: any) => (props: AppPropsType<NextRouter, {}>) =>
+                <App emotionCache={cache} {...props} />
         });
     /* eslint-enable */
 
-    const initialProps: DocumentInitialProps = await Document.getInitialProps(ctx);
-    const emotionStyles: EmotionCriticalToChunks = extractCriticalToChunks(initialProps.html);
-    const emotionStyleTags: JSX.Element[] = emotionStyles.styles.map((style) => (
-        <style
-            data-emotion={`${style.key} ${style.ids.join(' ')}`}
-            // eslint-disable-next-line react/no-unknown-property
-            key={style.key}
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{ __html: style.css }}
-        />
-    ));
+    const initialProps: DocumentInitialProps = await Document.getInitialProps(
+        ctx
+    );
+    const emotionStyles: EmotionCriticalToChunks = extractCriticalToChunks(
+        initialProps.html
+    );
+    const emotionStyleTags: JSX.Element[] = emotionStyles.styles.map(
+        (style) => (
+            <style
+                data-emotion={`${style.key} ${style.ids.join(' ')}`}
+                // eslint-disable-next-line react/no-unknown-property
+                key={style.key}
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{ __html: style.css }}
+            />
+        )
+    );
 
     return {
         ...initialProps,
