4e97ab530821c6b5dccdc142ac30fab6a7a6ba7c
fabianhabil <fabianhabilramdhan@gmail.com>
2023-01-21 00:27:50 +0700

    axios instance

--- frontend/src/api/axios-instance.ts
+++ frontend/src/api/axios-instance.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({
+const api = axios.create({
     baseURL: `${process.env.NEXT_PUBLIC_API_URL}/v1`,
     withCredentials: true
 });
 
-export default API;
+export default api;
