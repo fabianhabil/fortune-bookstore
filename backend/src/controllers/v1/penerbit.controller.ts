f2687dd785f9ad64bac5ce3f530a725a0e34677b
fabianhabil <fabianhabilramdhan@gmail.com>
2023-01-22 03:45:26 +0700

    get , create penerbit

--- backend/src/controllers/v1/penerbit.controller.ts
+++ backend/src/controllers/v1/penerbit.controller.ts
@@ -1,9 +1,17 @@
-import { Body, Get, JsonController, Post, Res } from 'routing-controllers';
+import {
+    Body,
+    CurrentUser,
+    Get,
+    JsonController,
+    Post,
+    Res
+} from 'routing-controllers';
 import { Service } from 'typedi';
 import { PenerbitService } from '../../services/penerbit.service';
 import { Response } from 'express';
 import { sendResponse } from '../../utils/api.util';
 import { PenerbitDTO } from '../../validations/penerbit.validation';
+import { UserPayload } from '../../typings/auth';
 
 @Service()
 @JsonController('/v1/penerbit')
@@ -21,8 +29,13 @@ export class PenerbitController {
     }
 
     @Post('/')
-    async createPenerbit(@Res() res: Response, @Body() dto: PenerbitDTO) {
-        await this.service.createPenerbit(dto);
+    async addPenerbit(
+        @Res() res: Response,
+        @Body() dto: PenerbitDTO,
+        @CurrentUser({ required: true }) user: UserPayload
+    ) {
+        const { userId } = user;
+        await this.service.addPenerbit(dto, userId);
 
         return sendResponse(res, {
             message: 'Successfully create one penerbit'
--- backend/src/services/penerbit.service.ts
+++ backend/src/services/penerbit.service.ts
@@ -1,16 +1,38 @@
+import { UserService } from './user.service';
 import { Service } from 'typedi';
 import { Penerbit } from '../database/entities/penerbit.entity';
 import type { PenerbitDTO } from '../validations/penerbit.validation';
+import { Errors } from '../utils/api.util';
 
 @Service()
 export class PenerbitService {
-    async getPenerbit() {
+    constructor(private readonly userService: UserService) {}
+
+    /*  Query SQL Get Penerbit
+        SELECT * FROM penerbit
+
+    */
+    async getPenerbit(): Promise<Penerbit[]> {
         const penerbit = await Penerbit.find();
 
         return penerbit;
     }
 
-    async createPenerbit(dto: PenerbitDTO) {
+    /*  Query SQL Add Penerbit
+        Make penerbit name into this format 'penerbit-name'
+        example, penerbitName = Good Company
+        then, the id become good-company
+
+        INSERT INTO TABLE penerbit VALUES(convert(body.penerbitName),
+                                          body.penerbitName)
+    */
+    async addPenerbit(dto: PenerbitDTO, userId: number) {
+        const isAdmin = await this.userService.isAdmin(userId);
+
+        if (!isAdmin) {
+            throw Errors.NO_PERMISSION;
+        }
+
         const penerbitId = dto.nama.replace(/ /g, '-').toLowerCase();
         const penerbit = Penerbit.create({ penerbitId, ...dto });
 
