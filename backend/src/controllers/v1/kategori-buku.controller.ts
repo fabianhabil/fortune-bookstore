e1aba578ff1bad9e5a1d068275e14d3190e2661c
fabianhabil <fabianhabilramdhan@gmail.com>
2023-01-22 03:45:09 +0700

    kategori buku get, create

--- backend/src/controllers/v1/kategori-buku.controller.ts
+++ backend/src/controllers/v1/kategori-buku.controller.ts
@@ -1,5 +1,42 @@
-import { JsonController } from "routing-controllers";
-import { Service } from "typedi";
+import {
+    Body,
+    CurrentUser,
+    Get,
+    JsonController,
+    Post,
+    Res
+} from 'routing-controllers';
+import { Service } from 'typedi';
+import { KategoriBukuService } from '../../services/kategori-buku.service';
+import { Response } from 'express';
+import { sendResponse } from '../../utils/api.util';
+import { KategoriBukuDTO } from '../../validations/kategori-buku.validation';
+import { UserPayload } from '../../typings/auth';
 
 @Service()
 @JsonController('/v1/kategori')
+export class KategoriBukuController {
+    constructor(protected service: KategoriBukuService) {}
+
+    @Get('/')
+    async getKategoriBuku(@Res() res: Response) {
+        const kategoriBuku = await this.service.get();
+
+        return sendResponse(res, {
+            message: 'Successfully found all penerbit',
+            data: { kategoriBuku }
+        });
+    }
+
+    @Post('/')
+    async addKategoriBuku(
+        @Res() res: Response,
+        @Body() dto: KategoriBukuDTO,
+        @CurrentUser({ required: true }) user: UserPayload
+    ) {
+        const { userId } = user;
+        await this.service.add(dto, userId);
+
+        return sendResponse(res, { message: 'Successfully add kategori buku' });
+    }
+}
--- backend/src/services/kategori-buku.service.ts
+++ backend/src/services/kategori-buku.service.ts
@@ -1,4 +1,35 @@
+/* eslint-disable max-len */
+import type { KategoriBukuDTO } from './../validations/kategori-buku.validation';
+import { KategoriBuku } from './../database/entities/kategori-buku.entity';
 import { Service } from 'typedi';
+import { UserService } from './user.service';
+import { Errors } from '../utils/api.util';
 
 @Service()
-export class KategoriBukuService {}
+export class KategoriBukuService {
+    constructor(private readonly userService: UserService) {}
+
+    /*  Query SQL Get All Kategori Buku
+        SELECT * FROM kategori_buku
+    */
+    async get(): Promise<KategoriBuku[]> {
+        const kategoriBuku = await KategoriBuku.find({});
+
+        return kategoriBuku;
+    }
+
+    /*  Query SQL Create Kategori Buku
+        INSERT INTO TABLE kategori_buku VALUES(body.namaKategoriBuku)
+    */
+    async add(dto: KategoriBukuDTO, userId: number) {
+        const isAdmin = await this.userService.isAdmin(userId);
+
+        if (!isAdmin) {
+            throw Errors.NO_PERMISSION;
+        }
+
+        const kategoriBuku = KategoriBuku.create({ ...dto });
+
+        await KategoriBuku.save(kategoriBuku);
+    }
+}
