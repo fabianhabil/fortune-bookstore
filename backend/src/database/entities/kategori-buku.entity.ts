9dbcfc59ae4814cc75becf37c95ce86703b31d75
fabianhabil <fabianhabilramdhan@gmail.com>
2023-01-22 03:44:22 +0700

    kategori buku entity fixed

--- backend/src/database/entities/kategori-buku.entity.ts
+++ backend/src/database/entities/kategori-buku.entity.ts
@@ -1,10 +1,20 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+    BaseEntity,
+    Column,
+    Entity,
+    OneToMany,
+    PrimaryGeneratedColumn
+} from 'typeorm';
+import { Buku } from './buku.entity';
 
 @Entity('kategori_buku')
 export class KategoriBuku extends BaseEntity {
     @PrimaryGeneratedColumn({ name: 'id_kategori_buku' })
-    penerbitId!: string;
+    kategoriBukuId!: number;
 
     @Column({ name: 'nama_kategori' })
     nama!: string;
+
+    @OneToMany(() => Buku, (buku) => buku)
+    buku!: Buku[];
 }
